337fc6492dc36dbabcb0eac6665adf30e74d4850
Ron <jaybayron400@gmail.com>
2024-12-24 22:49:02 +0800

    yak pro

---
+++ output/yakpro-po/obfuscated/index.php
@@ -0,0 +1,11 @@
+<?php
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.14  |
+    |              on 2024-12-22 07:06:48              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+/*
+
+*/
+ function sayHello() { return "\x68\x65\154\154\157\x21\40\x4a\141\x79"; } echo sayHello();
